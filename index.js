cf6ad4411f8b59efd846c95b69fd47343cb6a7e8
IsrakUAP <israk2301@gmail.com>
2023-11-06 22:35:02 +0600

    get and option method used to get data from collection by its id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,10 +30,20 @@ async function run() {
 
     const jobCollection = client.db('jobList').collection('category');
 
-    app.get('/category',async(req,res)=>{
-        const cursor = jobCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/category', async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/category/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        projection: { minPrice: 1, maxPrice: 1, deadline: 1 },
+      };
+      const result = await jobCollection.findOne(query,options);
+      res.send(result);
     })
 
 
@@ -48,10 +58,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
-    res.send('server is running')
+app.get('/', (req, res) => {
+  res.send('server is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
 })
