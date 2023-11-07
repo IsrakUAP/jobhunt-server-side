f8769ed3966472ce08028d2ae50bb21c8a2ad070
IsrakUAP <israk2301@gmail.com>
2023-11-08 01:37:44 +0600

    to get a single data by id get method used for jobcollection

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/category/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    })
+
     app.delete('/category/:id',async(req,res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
