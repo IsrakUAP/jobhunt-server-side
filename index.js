132a5d872b6bd1c533f8f11d9cfe5da5eca1cd56
IsrakUAP <israk2301@gmail.com>
2023-11-07 14:24:34 +0600

    get method added to find data from bidCollection database

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/bidList', async (req, res) => {
+      const cursor = bidCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
