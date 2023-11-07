1218bb12732ce4eb39ab16800f30f514f2fdc360
IsrakUAP <israk2301@gmail.com>
2023-11-08 04:36:32 +0600

    put method used to update status data for bidcollection

--- index.js
+++ index.js
@@ -100,6 +100,20 @@ async function run() {
       res.send(result);
     })
 
+    app.put('/bidList/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updateStatus = req.body;
+      const update = {
+        $set: {
+          status: updateStatus.status
+        }     
+      }
+      const result = await bidCollection.updateOne(filter,update,options);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
