bb669cf9724535dfe0de7fa1ac933fbb5466a10a
IsrakUAP <israk2301@gmail.com>
2023-11-08 12:21:21 +0600

    jwt token added to secure the authentication for user

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -30,6 +36,19 @@ async function run() {
 
     const jobCollection = client.db('jobList').collection('category');
     const bidCollection = client.db('jobList').collection('bidList')
+
+
+    app.post('/jwt',async(req,res) =>{
+        const user = req.body;
+        console.log(user);
+        const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({success: true});
+    })
 
 
     app.post('/category', async (req, res) => {
@@ -113,6 +132,7 @@ async function run() {
       const result = await bidCollection.updateOne(filter,update,options);
       res.send(result);
     })
+
 
 
 
